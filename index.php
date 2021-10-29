449c9d97e9121989424ef4f5761a83c7420b67d1
Berta Pluma <bertapsan@gmail.com>
2021-10-29 15:58:08 +0200

    Add screenshot

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Bertapsan Web Starter Kit</title>
+    <title>Bertapsan PHP-M2 Task</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@100;300&display=swap" rel="stylesheet">
